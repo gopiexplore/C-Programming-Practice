8f15c2d6dfde16039887967836fc1e4dbed3392b
gopii8 <gopithammisetti6@gmail.com>
2023-02-16 14:37:16 +0530

    Sum of Odd Numbers From 1 to N

---
+++ sumOfAllOdd1toN.c
@@ -0,0 +1,15 @@
+//8.Find sum of all odd numbers between 1 to n.
+#include <stdio.h>
+void main(){
+    int n,sum,i;
+    printf("Enter n to find all even numbers sum 1 to n ");
+    scanf(" %d",&n);
+    for(i=1;i<=n;i++){
+        if(i%2!=0){
+            sum=sum+i;
+            
+        }
+     
+    }
+    printf("%d ",sum); 
+}
