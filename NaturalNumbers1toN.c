2167e2a1866d7dc1c7181c031a129afafcbb21a7
gopii8 <gopithammisetti6@gmail.com>
2023-02-16 14:11:57 +0530

    Natural Numbers 1 to n

---
+++ NaturalNumbers1toN.c
@@ -0,0 +1,11 @@
+//1.Print all Natural Numbers form 1 to N.
+#include <stdio.h>
+void main()
+{
+    int n,i;
+    printf("Enter number ");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++){
+        printf("%d ",i);
+    }
+}
