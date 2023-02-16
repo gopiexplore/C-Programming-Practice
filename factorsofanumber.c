bbaa11bbaccf8ce587a61c771f5e097ba4eb49c9
gopii8 <gopithammisetti6@gmail.com>
2023-02-16 17:17:46 +0530

    Factors of a Number

---
+++ factorsofanumber.c
@@ -0,0 +1,12 @@
+//22.Find all factors of a Number.
+#include <stdio.h>
+void main(){
+    int n,i;
+    printf("Enter a Number to find all factors of a Number");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++){
+        if(n%i==0){
+            printf("%d ",i);
+        }
+    }
+}
